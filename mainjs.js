254d081052be6eef638ade87b4801527c00152d2
unconquerablepotato <unconquerablepotato@gmail.com>
2016-09-19 22:25:52 -0400

    so much test.

--- mainjs.js
+++ mainjs.js
@@ -3,7 +3,7 @@ function aboutClick()
 	//var about = document.getElementById('about');
 	//var links = document.getElementById('links');
 	
-	document.getElementById("text").innerHTML = "Hey hey hey.<br><br>I’m Omnipotent “Omni” Potato, an independent game developer from the US.<br> I have worked on programming for 5 years, starting with very basic level games and concepts and then working my way up to full games made with GM:S and Java.<br><br>I like to experiment with weird and different game mechanics that make the gameplay of my games fun and original.<br> I’ve had some of my early games featured on Scratch (a program by the MIT Media Lab) as well as get thousands of plays.<br> My top games on GameJolt have 4+ star reviews and I work on more projects every day!";
+	document.getElementById("text").innerHTML = "hoi. filler about me. lol.";
 	
 	linetoworkwith = document.getElementById("text");
 	
@@ -36,45 +36,3 @@ function gamesClick()
 	linetoworkwith.offsetWidth = linetoworkwith.offsetWidth;
 	linetoworkwith.classList.add("fadeInDown");
 }
-
-
-
-
-
-
-
-
-
-var titlevar = Math.floor((Math.random() * 7) + 1);
-
-if (titlevar == 1)
-	{
-		document.title = "Omni Game Design"
-	}
-
-if (titlevar == 2)
-	{
-		document.title = "xX::OmniGameDesign::Xx"
-	}
-
-if (titlevar == 3)
-	{
-		document.title = "OmniLameDesign"
-	}
-
-if (titlevar == 4)
-	{
-		document.title = "()mn! g4m3 dE516n"
-	}
-if (titlevar == 5)
-	{
-		document.title = "OmniNameDesign"
-	}
-if (titlevar == 6)
-	{
-		document.title = "/* OmniGameDesign */"
-	}
-if (titlevar == 7)
-	{
-		document.title = "<-- I'm with stupid"
-	}
